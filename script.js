54bb5b9243372ef40bfb42b54ef8f0dc6d349bd8
EskanirX <125078699+EskanirX@users.noreply.github.com>
2023-04-28 19:06:25 +0400

    finish

---
+++ script.js
@@ -0,0 +1,23 @@
+// pirveli
+let a = 100;
+if (a < 50) {
+  console.log("naklebia 50ze");
+}
+if (a > 20) {
+  console.log("metia 20ze");
+} else "shecdoma";
+
+// meore
+let b = "mariami";
+if ((b = "mariami")) {
+  console.log(true);
+} else false;
+// mesame
+let c = "mariami";
+switch (c) {
+  case "mariami":
+    console.log(true);
+    break;
+  default:
+    console.log("error");
+}
